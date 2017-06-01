3d0aefbb2e20eb72f78f2aaff380ab015861a870
Toby-Jackson <Toby-Jackson@users.noreply.github.com>
2017-06-02 09:00:50 +1200

    Update rODBd.py
    converts numpy array to list because I am a fool, and numpy scares me

--- src/rODBd.py
+++ src/rODBd.py
@@ -1,6 +1,6 @@
 from odbAccess import *
 
-odb = openOdb(path='C:\\Users\\bioengsu\\Desktop\\dssr2017ABI-tjac799\\data\\HE2St.odb')
+odb = openOdb('HE2St.odb')
 
 step = odb.steps['STEP-2']
 
@@ -12,11 +12,11 @@ all_nodes = odb.rootAssembly.nodeSets[' ALL NODES'].nodes[0]
 n_coord = {}
 n_disp={}
 for node in all_nodes:
-    n_coord[node.label] = node.coordinates
-    n_disp[U.values[int(node.label)-1].nodeLabel] = U.values[int(node.label)-1].data[0]
+    n_coord[node.label] = list(node.coordinates)
+    n_disp[U.values[int(node.label)-1].nodeLabel] = list(U.values[int(node.label)-1].data[:])
 # n_coord
 
 # print n_coord.keys()
 
 
-print n_disp
+print n_disp.keys()
