bcd20e6f444d02241c0e78b7e63b7b0cb4ae64d7
Toby Jackson <tjac799@aucklanduni.ac.nz>
2017-06-01 16:03:15 +1200

    abaqus python script extract node and node_disp dictionary from odb file, not validated

---
+++ src/rODBd.py
@@ -0,0 +1,22 @@
+from odbAccess import *
+
+odb = openOdb(path='C:\\Users\\bioengsu\\Desktop\\dssr2017ABI-tjac799\\data\\HE2St.odb')
+
+step = odb.steps['STEP-2']
+
+frame = step.frames[-1]
+
+U = frame.fieldOutputs['U']
+
+all_nodes = odb.rootAssembly.nodeSets[' ALL NODES'].nodes[0]
+n_coord = {}
+n_disp={}
+for node in all_nodes:
+    n_coord[node.label] = node.coordinates
+    n_disp[node.label] = U.values[int(node.label)-1].data
+# n_coord
+
+# print n_coord.keys()
+
+
+print n_disp
