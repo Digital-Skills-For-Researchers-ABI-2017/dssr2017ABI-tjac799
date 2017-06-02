ca08e7ef498679b7d247ce414d7283032dd3fbcf
Toby Jackson <tjac799@aucklanduni.ac.nz>
2017-06-02 14:57:23 +1200

    Mapped displacement field to undeformed geom, visual check is good, need to check on node numbering individually

--- src/rODBd.py
+++ src/rODBd.py
@@ -1,5 +1,5 @@
 from odbAccess import *
-
+from operator import add
 odb = openOdb('HE2St.odb')
 
 step = odb.steps['STEP-2']
@@ -11,12 +11,20 @@ U = frame.fieldOutputs['U']
 all_nodes = odb.rootAssembly.nodeSets[' ALL NODES'].nodes[0]
 n_coord = {}
 n_disp={}
+# extract undeformed nodes, extract deformation field
 for node in all_nodes:
     n_coord[node.label] = list(node.coordinates)
     n_disp[U.values[int(node.label)-1].nodeLabel] = list(U.values[int(node.label)-1].data[:])
-# n_coord
 
-# print n_coord.keys()
+# map deformation field to undeformed nodes to create the deformed geometry
+n_def = {}
+for node in n_disp.keys():
+    n_def[node] = map(add,n_coord[node],n_disp[node])
+    
 
+print n_def
 
-print n_disp.keys()
+f_def = open("HE2StDefGeo.txt",'w')
+# write deformed geometry to file
+for node in n_def.keys():
+    f_def.write(str(node) + ',   ' + str(n_def[node][0])+ ',   ' + str(n_def[node][1]) + ',   ' + str(n_def[node][2])+'\n')
